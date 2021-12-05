3c94090dae3958642aa181032767d61bddeae2f1
Chick3nTheCoder <krishsaxena.ca@gmail.com>
2021-12-05 01:47:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -132,10 +132,10 @@ module.exports = {
 
 client.on('messageCreate', async message => {
     if (message.embeds.length !== 0) {
-        // if (message.channel.id === '916919202105946142' && message.embeds[0].footer.text.includes('252980043511234560')) message.delete();
-        
+        if (message.channel.id === '916919202105946142' && message.embeds[0].footer.text.includes('252980043511234560')) message.delete();
+        console.log(message.embeds[0].footer.text);
         if (message.channel.id === '916919202105946142' && (message.embeds[0].description.includes('751565931746033745')) || message.embeds[0].description.includes('751565931746033745')) message.delete();
-        console.log(message.embeds[0].description);
+        
     }
     /*
     if (message.channel.id == '') {
