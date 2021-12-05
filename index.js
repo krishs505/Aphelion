807491c5516a7b12f7f9a6a47e614c3d0192d834
Chick3nTheCoder <krishsaxena.ca@gmail.com>
2021-12-05 00:41:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -132,6 +132,7 @@ module.exports = {
 
 client.on('messageCreate', async message => {
     if (message.channel.id === '916919202105946142' && message.embeds[0].footer.text.includes('252980043511234560')) message.delete();
+    if (message.channel.id === '916919202105946142' && (message.embeds[0].description.includes('751565931746033745')) || message.embeds[0].description.includes('751565931746033745')) message.delete();
 
     /*
     if (message.channel.id == '') {
