efbc7622a585abc46de7071b04901d3f445cde02
Chick3nTheCoder <krishsaxena.ca@gmail.com>
2021-12-05 01:44:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -131,12 +131,12 @@ module.exports = {
 };
 
 client.on('messageCreate', async message => {
-    // if (message.embeds.length !== 0) {
+    if (message.embeds.length !== 0) {
         // if (message.channel.id === '916919202105946142' && message.embeds[0].footer.text.includes('252980043511234560')) message.delete();
         
-        if (message.embeds.length !== 0 && message.channel.id === '916919202105946142' && (message.embeds[0].description.includes('751565931746033745')) || message.embeds[0].description.includes('751565931746033745')) message.delete();
-        if (message.embeds.length !== 0) console.log(message.embeds[0].description);
-    // }
+        if (message.channel.id === '916919202105946142' && (message.embeds[0].description.includes('751565931746033745')) || message.embeds[0].description.includes('751565931746033745')) message.delete();
+        console.log(message.embeds[0].description);
+    }
     /*
     if (message.channel.id == '') {
         message.react("👍")
