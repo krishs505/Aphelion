8752a536b0c012aa46e3b40ae5ce2df6f93df259
Chick3nTheCoder <krishsaxena.ca@gmail.com>
2022-02-21 14:08:39 -0500

    qwer

---
+++ commands/games/wordle.js
@@ -0,0 +1,14 @@
+const { bot } = require("../../index")
+
+module.exports = {
+    name: 'wordle',
+    description: 'Solves a wordle.',
+    usage: ' ',
+    cooldown: 0,
+    od: true,
+    execute(message, args) {
+
+        
+
+    }
+}
