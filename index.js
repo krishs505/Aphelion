c5ceb0503378009450472b01dfb720f9e7456d64
KiheiCodes <krishsaxena.ca@gmail.com>
2022-07-22 02:51:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let BotName = 'Aphelion';
 const DJSVersion = '14.0.2';
 const BotSupportLink = 'https://discord.gg/';
 const BotDev = '252980043511234560';
-
+//
 // git push heroku main:main
 // heroku logs -n 50
 
